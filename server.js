636e2ac30e1744c2a8ffd52410638debb0ff660d
Tommy23gun <tommaso.rizzardi@gmail.com>
2022-10-01 17:06:03 +0200

    first deploy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: false,
   })
